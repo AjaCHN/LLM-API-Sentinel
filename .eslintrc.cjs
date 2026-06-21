af83c49dc5fa171a63893dc69a2129882d8a7bc5
AjaCHN <AjaCHN@users.noreply.github.com>
2026-06-21 07:28:34 +0000

    feat: 优化 Web 项目原型
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- .eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ["next/core-web-vitals", "next/typescript"],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }]
-  },
-  ignorePatterns: ["node_modules/", "out/", ".next/", "jest.setup.cjs"]
-};
